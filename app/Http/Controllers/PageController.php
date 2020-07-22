81fde74b7a58c29a88dc829e7373e2e4538a23d3
Nikita <n.maimusov@involta.ru>
2020-07-22 23:27:30 +0300

    Добавил вывод турниров на главную

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Tournament;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('status', 'PUBLISHED')->where('featured', true)->whereNotNull('image')->orderBy('id', 'DESC')->limit(6)->get();
-        return view('main.index',compact('posts'));
+        $tournaments        = Tournament::latest()->limit(3)->get();
+        $posts              = Post::where('status', 'PUBLISHED')->where('featured', true)->whereNotNull('image')->orderBy('id', 'DESC')->limit(6)->get();
+        return view('main.index',compact('posts','tournaments'));
     }
 }
